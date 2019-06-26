5ff8ca275936a838f0b9ee801d7197174b24296a
acheng <964067013@qq.com>
2019-06-26 18:36:41 +0800

    fix

---
+++ login/forms.py
@@ -0,0 +1,51 @@
+from django import forms
+from captcha.fields import CaptchaField
+
+
+class UserForm(forms.Form):
+    username = forms.CharField(label="用户名", max_length=128, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    password = forms.CharField(label="密码", max_length=256, widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    captcha = CaptchaField(label='验证码')
+
+
+class RegisterForm(forms.Form):
+    gender = (
+        ('male', "男"),
+        ('female', "女"),
+    )
+    username = forms.CharField(label="用户名", max_length=128, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    password1 = forms.CharField(label="密码", max_length=256, widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    password2 = forms.CharField(label="确认密码", max_length=256, widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    email = forms.EmailField(label="邮箱地址", widget=forms.EmailInput(attrs={'class': 'form-control'}))
+    sex = forms.ChoiceField(label='性别', choices=gender)
+    captcha = CaptchaField(label='验证码')
+
+
+class ModifyInfoForm(forms.Form):
+    password1 = forms.CharField(label="新密码", max_length=256, widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    password2 = forms.CharField(label="确认密码", max_length=256, widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+
+
+class UpTransForm(forms.Form):
+
+    TRANSTYPE=(
+        ('survey', '填写问卷'),
+        ('morning', '叫早服务'),
+        ('takeout', '带饭服务'),
+        ('express', '代领快递'),
+        ('drug', '代买药品'),
+    )
+    # type = forms.ChoiceField(label=u'类型', widget=forms.ChoiceField())
+    bonus = forms.IntegerField(label="分数", widget=forms.TextInput(attrs={'class': 'form-control'}))
+    # uploader = forms.CharField(label="发布者", max_length=128, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    phone = forms.CharField(label="联系电话", max_length=32, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    detail = forms.CharField(label="任务详情", max_length=512, widget=forms.Textarea(attrs={'class': 'form-control', 'style': 'resize:none'}))
+    # c_time = forms.DateTimeField(label="发布日期", max_length=128, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    d_time = forms.DateTimeField(label="截止日期", widget=forms.TextInput(attrs={'class': 'form-control'}))
+    # is_accept = forms.BooleanField(label="接受情况", max_length=128, widget=forms.BInput(attrs={'class': 'form-control'}))
+    # is_finish = forms.BooleanField(label="完成情况", max_length=128, widget=forms.TextInput(attrs={'class': 'form-control'}))
+
+
+class FindTransForm(forms.Form):
+    accept_trans = forms.IntegerField(label="任务Id", widget=forms.TextInput(attrs={'class': 'form-control'}))
+
--- login/views.py
+++ login/views.py
@@ -133,3 +133,267 @@ def up_trans(request):
 
 #########################################################################
 
+#########################################################################undo2
+def find_trans(request):
+    if request.method == "GET":
+        trans_info = []
+        '''
+        trans_info['trans_num'] = []
+        trans_info['type'] = []
+        trans_info['bonus'] = []
+        trans_info['uploader'] = []
+        trans_info['phone'] = []
+        trans_info['detail'] = []
+        trans_info['d_time'] = []
+        trans_info['is_accept'] = []
+        trans_info['acceptor'] = []
+        trans_info['is_finish'] = []
+        '''
+        lista = Transaction.objects.values_list('trans_num', flat=True)
+        listb = Transaction.objects.values_list('type', flat=True)
+        listc = Transaction.objects.values_list('bonus', flat=True)
+        listd = Transaction.objects.values_list('uploader', flat=True)
+        liste = Transaction.objects.values_list('phone', flat=True)
+        listf = Transaction.objects.values_list('detail', flat=True)
+        listg = Transaction.objects.values_list('d_time',  flat=True)
+        listh = Transaction.objects.values_list('is_accept', flat=True)
+        listi = Transaction.objects.values_list('acceptor', flat=True)
+        listj = Transaction.objects.values_list('is_finish', flat=True)
+        for i in range(len(lista)):
+            dict = {
+                '任务编号': lista[i],
+                '任务类型': get_type(listb[i]),
+                '积分奖励': listc[i],
+                '发布者': listd[i],
+                '联系电话': liste[i],
+                '任务内容': listf[i],
+                '截止日期': listg[i],
+                '接受状态': listh[i],
+                '执行人': listi[i],
+                '完成状态': listj[i]
+            }
+            trans_info.append(
+               dict
+            )
+            '''
+            trans_info['trans_num'].append(lista[i])
+            trans_info['type'].append(listb[i])
+            trans_info['bonus'].append(listc[i])
+            trans_info['uploader'].append(listd[i])
+            trans_info['phone'].append(liste[i])
+            trans_info['detail'].append(listf[i])
+            trans_info['d_time'].append(listg[i])
+            trans_info['is_accept'].append(listh[i])
+            trans_info['acceptor'].append(listi[i])
+            trans_info['is_finish'].append(listj[i])
+            '''
+        '''
+        trans_num = Transaction.objects.values_list('trans_num', flat=True)
+        type = Transaction.objects.values_list('type', flat=True)
+        bonus = Transaction.objects.values_list('bonus', flat=True)
+        uploader = Transaction.objects.values_list('uploader', flat=True)
+        phone = Transaction.objects.values_list('phone', flat=True)
+        detail = Transaction.objects.values_list('detail', flat=True)
+        d_time = Transaction.objects.values_list('d_time',  flat=True)
+        is_accept = Transaction.objects.values_list('is_accept', flat=True)
+        acceptor = Transaction.objects.values_list('acceptor', flat=True)
+        is_finish = Transaction.objects.values_list('is_finish', flat=True)
+        '''
+        temp = {
+            'key': trans_info
+        }
+        return render(request, "login/find_trans.html", temp)
+    elif request.method == "POST":
+        temp_trans_num = request.POST.get('accept')
+        trans = models.Transaction.objects.get(trans_num=temp_trans_num)
+        request.session['trans_num'] = trans.trans_num
+        request.session['trans_type'] = get_type(trans.type)
+        request.session['trans_bonus'] = trans.bonus
+        request.session['trans_uploader'] = trans.uploader
+        request.session['trans_phone'] = trans.phone
+        request.session['trans_detail'] = trans.detail
+        return redirect('/accept_trans/')
+    find_trans_form = FindTransForm()
+    return render(request, 'login/find_trans.html', locals())
+
+def get_type(var):
+    if var == 'survey':
+        return '填写问卷'
+    elif var == 'morning':
+        return '叫早服务'
+    elif var == 'takeout':
+        return '带饭服务'
+    elif var == 'express':
+        return '代领快递'
+    elif var == 'drug':
+        return '代买药品'
+    else:
+        return '测试样例'
+#########################################################################
+
+
+def membership(request):
+    pass
+    return render(request, "login/membership.html")
+
+
+def modify_info(request):
+    # if request.session.get('is_login', None):
+    if request.method == "POST":
+        modify_info_form = ModifyInfoForm(request.POST)
+        if modify_info_form.is_valid():  # 获取数据
+            password1 = modify_info_form.cleaned_data['password1']
+            password2 = modify_info_form.cleaned_data['password2']
+            if password1 != password2:  # 判断两次密码是否相同
+                message = "两次输入的密码不同！"
+                return render(request, 'login/modify_info.html', locals())
+            else:
+                user = models.User.objects.get(name=request.session.get('user_name'))
+                user.password = password1
+                user.save()
+                request.session.flush()
+                message = "修改成功！"
+                return render(request, 'login/index.html', {'message': message})
+    modify_info_form = ModifyInfoForm()
+    return render(request, 'login/modify_info.html', locals())
+
+
+def my_transaction(request):
+    pass
+    return render(request, 'login/my_transaction.html')
+
+
+def msg_center(request):
+    pass
+    return render(request, 'login/msg_center.html')
+
+
+def accept_trans(request):
+    dict = {
+        '任务编号': request.session['trans_num'],
+        '任务类型': request.session['trans_type'],
+        '积分奖励': request.session['trans_bonus'],
+        '发布者': request.session['trans_uploader'],
+        '联系电话': request.session['trans_phone'],
+        '任务内容': request.session['trans_detail'],
+        # '截止日期': request.session['trans_d_time'],
+    }
+    # print(dict)
+    temp_trans = models.Transaction.objects.get(trans_num=request.session['trans_num'])
+    temp_trans.acceptor = request.session['user_name']
+    temp_trans.is_accept = True
+    temp_trans.save()
+    return render(request, 'login/accept_trans.html', dict)
+
+
+def my_doing(request):
+    if request.method == "GET":
+        my_doing = models.Transaction.objects.filter(Q(acceptor=request.session.get('user_name')) & Q(is_finish=False)).values_list()
+        my_doing_trans = []
+        for i in range(len(my_doing)):
+            dict = {
+                '任务编号': my_doing[i][0],
+                '任务类型': get_type(my_doing[i][1]),
+                '积分奖励': my_doing[i][2],
+                '发布者': my_doing[i][3],
+                '联系电话': my_doing[i][4],
+                '任务内容': my_doing[i][5],
+                '截止日期': my_doing[i][6],
+                '接受状态': my_doing[i][8],
+                '执行人': my_doing[i][9],
+                '完成状态': my_doing[i][10]
+            }
+            my_doing_trans.append(
+                dict
+            )
+        return render(request, 'login/my_doing.html', {'key': my_doing_trans})
+    elif request.method == "POST":
+        temp_trans = models.Transaction.objects.get(trans_num=request.POST.get('cancel'))
+        temp_trans.acceptor = 'None'
+        temp_trans.is_accept = False
+        temp_trans.save()
+        message = "取消成功"
+        return render(request, 'login/my_transaction.html', {'message': message})
+    return render(request, 'login/my_doing.html')
+
+
+def my_finish(request):
+    if request.method == "GET":
+        my_finish = models.Transaction.objects.filter(Q(acceptor=request.session.get('user_name')) & Q(is_finish=True)).values_list()
+        my_finish_trans = []
+        for i in range(len(my_finish)):
+            dict = {
+                '任务编号': my_finish[i][0],
+                '任务类型': get_type(my_finish[i][1]),
+                '积分奖励': my_finish[i][2],
+                '发布者': my_finish[i][3],
+                '联系电话': my_finish[i][4],
+                '任务内容': my_finish[i][5],
+                '截止日期': my_finish[i][6],
+                '接受状态': my_finish[i][8],
+                '执行人': my_finish[i][9],
+                '完成状态': my_finish[i][10]
+            }
+            my_finish_trans.append(
+                dict
+            )
+        return render(request, 'login/my_finish.html', {'key': my_finish_trans})
+    return render(request, 'login/my_finish.html')
+
+
+def my_upload(request):
+    if request.method == "GET":
+        my_upload = models.Transaction.objects.filter(uploader=request.session.get('user_name')).values_list()
+        my_upload_trans = []
+        for i in range(len(my_upload)):
+            dict = {
+                '任务编号': my_upload[i][0],
+                '任务类型': get_type(my_upload[i][1]),
+                '积分奖励': my_upload[i][2],
+                '发布者': my_upload[i][3],
+                '联系电话': my_upload[i][4],
+                '任务内容': my_upload[i][5],
+                '截止日期': my_upload[i][6],
+                '接受状态': my_upload[i][8],
+                '执行人': my_upload[i][9],
+                '完成状态': my_upload[i][10]
+            }
+            my_upload_trans.append(
+                dict
+            )
+        return render(request, 'login/my_upload.html', {'key': my_upload_trans})
+    elif request.method == "POST":
+        temp_trans = models.Transaction.objects.get(trans_num=request.POST.get('delete'))
+        temp_trans.is_finish = True
+        temp_trans.save()
+        message = "操作成功！"
+        return render(request, 'login/my_transaction.html', {'message': message})
+    return render(request, 'login/my_upload.html')
+
+
+def delete_trans(request):
+    if request.method == "GET":
+        my_upload = models.Transaction.objects.filter(uploader=request.session.get('user_name')).values_list()
+        my_upload_trans = []
+        for i in range(len(my_upload)):
+            dict = {
+                '任务编号': my_upload[i][0],
+                '任务类型': get_type(my_upload[i][1]),
+                '积分奖励': my_upload[i][2],
+                '发布者': my_upload[i][3],
+                '联系电话': my_upload[i][4],
+                '任务内容': my_upload[i][5],
+                '截止日期': my_upload[i][6],
+                '接受状态': my_upload[i][8],
+                '执行人': my_upload[i][9],
+                '完成状态': my_upload[i][10]
+            }
+            my_upload_trans.append(
+                dict
+            )
+        return render(request, 'login/delete_trans.html', {'key': my_upload_trans})
+    elif request.method == "POST":
+        temp_trans = models.Transaction.objects.get(trans_num=request.POST.get('delete')).delete()
+        message = "删除成功！"
+        return render(request, 'login/my_transaction.html', {'message': message})
+    return render(request, 'login/delete_trans.html')
